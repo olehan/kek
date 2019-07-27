b54bd058248807b790afe1d56c1cb1d9c8b73f35
Alikhan Arshabekov <alihan.arshabekov@gmail.com>
2019-07-27 21:45:11 +0600

    fix(printer): use PrintTemplateMap instead of PrintTemplate

--- printer/service.go
+++ printer/service.go
@@ -6,43 +6,48 @@ import (
     "github.com/olehan/kek/ds"
 )
 
-func (p *Printer) Print(v ...interface{}) (int, error) {
+func (p *Printer) Print(v ...interface{}) *Printer {
     state, fc := p.initState()
     defer p.reset(state)
     p.formatter.Print(fc, v...)
-    return p.fc.Writer.Write(state.Buffer)
+    p.fc.Writer.Write(state.Buffer)
+    return p
 }
 
-func (p *Printer) Println(v ...interface{}) (int, error) {
+func (p *Printer) Println(v ...interface{}) *Printer {
     state, fc := p.initState()
     defer p.reset(state)
     p.formatter.Print(fc, v...)
     state.Buffer.WriteNewLine()
-    return p.fc.Writer.Write(state.Buffer)
+    p.fc.Writer.Write(state.Buffer)
+    return p
 }
 
-func (p *Printer) PrintT(template string, v ...interface{}) (int, error) {
+func (p *Printer) PrintT(template string, v ...interface{}) *Printer {
     state, fc := p.initState()
     defer p.reset(state)
     p.formatter.PrintTemplate(fc, template, v...)
     state.Buffer.WriteNewLine()
-    return p.fc.Writer.Write(state.Buffer)
+    p.fc.Writer.Write(state.Buffer)
+    return p
 }
 
-func (p *Printer) PrintTM(template string, v ds.Map) (int, error) {
+func (p *Printer) PrintTM(template string, v ds.Map) *Printer {
     state, fc := p.initState()
     defer p.reset(state)
-    p.formatter.PrintTemplate(fc, template, v)
+    p.formatter.PrintTemplateMap(fc, template, v)
     state.Buffer.WriteNewLine()
-    return p.fc.Writer.Write(state.Buffer)
+    p.fc.Writer.Write(state.Buffer)
+    return p
 }
 
-func (p *Printer) PrintTKV(template string, keyValues ...interface{}) (int, error) {
+func (p *Printer) PrintTKV(template string, keyValues ...interface{}) *Printer {
     state, fc := p.initState()
     defer p.reset(state)
-    p.formatter.PrintTemplate(fc, template, keyValues...)
+    p.formatter.PrintTemplateKeyValue(fc, template, keyValues...)
     state.Buffer.WriteNewLine()
-    return p.fc.Writer.Write(state.Buffer)
+    p.fc.Writer.Write(state.Buffer)
+    return p
 }
 
 func (p *Printer) getFormatterState(ps *pool.PoolState) *formatters.FormatterConfig {
