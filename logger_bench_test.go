89012044ed0f8e8fbe347b4f13574904c424c9a9
Alikhan Arshabekov <alihan.arshabekov@gmail.com>
2019-07-27 21:46:04 +0600

    test(template-printer): add benchmarks

--- logger_bench_test.go
+++ logger_bench_test.go
@@ -1,6 +1,7 @@
 package kek
 
 import (
+    "github.com/olehan/kek/ds"
     "github.com/olehan/kek/formatters/suggared"
     "os"
     "testing"
@@ -16,5 +17,42 @@ func BenchmarkLogger_Println(b *testing.B) {
     b.ReportAllocs()
     for i := 0; i < b.N; i++ {
         _testLogger.Debug.Println(_testLoggerValues...)
+    }
+}
+
+func BenchmarkLogger_PrintT(b *testing.B) {
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _testLogger.Debug.PrintT("val {} format {} template- {} asasd {} - {}sdasd {},,, {}", _testLoggerValues...)
+    }
+}
+
+func BenchmarkLogger_PrintTM(b *testing.B) {
+    values := ds.Map{
+        "key1": "asdafgagwrt",
+        "key2": 34635636.346356,
+        "key3": 34234624624,
+        "key4": "asd",
+    }
+
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _testLogger.Debug.PrintTM("val {{key1}} format {{key2}} template- {{key3}} asasd {{key4}}-asd", values)
+    }
+}
+
+func BenchmarkLogger_PrintTKV(b *testing.B) {
+    values := []interface{}{
+        "key1", "value1",
+        "key2", "value2",
+        "key3", "value3",
+        "key4", "value4",
+        "unknown", 25245,
+        245245,
+    }
+
+    b.ReportAllocs()
+    for i := 0; i < b.N; i++ {
+        _testLogger.Debug.PrintTKV("there is a {{key1}} and {{key2}}, also {{key3}} asdo-a- {{key4}}", values...)
     }
 }
