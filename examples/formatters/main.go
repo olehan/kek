6d68cd8b139e7f17b2f472277ba1dfef711ed3b7
Alikhan Arshabekov <alihan.arshabekov@gmail.com>
2019-07-28 18:46:53 +0600

    refactor(examples): add struct log in formattter example

--- examples/formatters/main.go
+++ examples/formatters/main.go
@@ -4,14 +4,29 @@ import (
     "github.com/olehan/kek"
     "github.com/olehan/kek/formatters/minified"
     "github.com/olehan/kek/formatters/sugared"
+    "os"
 )
 
 func main() {
-    minifiedLogger := kek.NewLogger("name").SetFormatter(minified.Formatter)
+    factory := kek.NewFactory(os.Stdout, minified.Formatter)
+
+    minifiedLogger := factory.NewLogger("name")
     minifiedLogger.SetRandomNameColor()
     minifiedLogger.Debug.Println("hello, kek!")
+    minifiedLogger.Debug.PrintSKV("here goes structures",
+        "url", "http://127.0.0.1:1110",
+        "jwt", "v75onlb3iv3i7t5",
+        "salt", 123,
+    )
 
-    sugaredLogger := kek.NewLogger("name").SetFormatter(sugared.Formatter)
+    sugaredLogger := factory.NewLogger("name").SetFormatter(sugared.Formatter)
     sugaredLogger.SetRandomNameColor()
     sugaredLogger.Info.Println("hello, not minified logger")
+    sugaredLogger.Info.PrintSKV("cache state",
+        "keys", 92334,
+        "misses", 573,
+        "matches", 24247754,
+        "ttl", 1300.1220,
+        "active", true,
+    )
 }
