8437f1908fb8eb95d4b7c4bfde566778c0ec8c3b
Alikhan Arshabekov <alihan.arshabekov@gmail.com>
2019-07-30 13:47:07 +0600

    fix(ci): remove sudo=false

--- pool/service_test.go
+++ pool/service_test.go
@@ -10,7 +10,6 @@ func TestPool(t *testing.T) {
     s := _testPool.Get()
     s.Buffer.WriteSpace()
     _testPool.Set(s)
-    s = _testPool.Get()
     if string(s.Buffer) != " " {
         panic("Buffer from state has no assigned space")
     }
